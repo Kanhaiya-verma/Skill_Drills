9597fb123e0fa8e4e012848a6ea0d31debc077d6
Kanhaiya-verma <Kanhaiyaverma2580@gmail.com>
2022-07-25 15:27:23 +0530

    Edit naviagtion bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import Sidebar from "./Html/Sidebar";
 function App() {
-  return <div>
-  <h1> Skill Drills</h1>
-  </div>;
+  return (
+    <div>
+      <h1> Skill Drills</h1>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
--- src/Html/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const index = () => {
-  return <div>This Is Html File</div>;
-};
-
-export default index;
